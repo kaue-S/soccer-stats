acaea68d2ec0d872fe3c1cb4181f9b7eaf3bdd73
kaue-S <kauesilva38@hotmail.com>
2024-06-04 11:36:50 -0300

    Update page.jsx

--- app/brasileirao/page.jsx
+++ app/brasileirao/page.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+
 export default function Brasileirao() {
+
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  fetch(`https://api.football-data.org/v4/competitions/PL`)
+  .then(response => response.json()) // Parse the JSON response
+  .then(data => {
+    console.log(data); // Use the fetched data
+  })
+  .catch(error => {
+    console.error(error); // Handle any errors
+  });
+
+
+
   return (
     <main>
       <h1>brasileirao</h1>
+
+
     </main>
   )
 }
