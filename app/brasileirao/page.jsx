85aca799fd5f3fc4a1ba146939fe4ffd56e4352f
kaue-S <kauesilva38@hotmail.com>
2024-06-04 14:06:30 -0300

    comit

--- app/brasileirao/page.jsx
+++ app/brasileirao/page.jsx
@@ -1,26 +1,33 @@
+
+import Image from 'next/image';
 import React from 'react'
 
 
-export default function Brasileirao() {
+export default async function Brasileirao() {
 
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  // const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
-  fetch(`https://api.football-data.org/v4/competitions/PL`)
-  .then(response => response.json()) // Parse the JSON response
-  .then(data => {
-    console.log(data); // Use the fetched data
-  })
-  .catch(error => {
-    console.error(error); // Handle any errors
-  });
+   const resposta = await fetch('https://apiv3.apifootball.com/?action=get_leagues&country_id=27&APIkey=8fb6c79632ee46c334a924cd01221430ce944e9b920eaea51bbbfe887b55bc85')
+   const dados = await resposta.json();
+   console.log(dados);
 
-
-
-  return (
+return (
     <main>
-      <h1>brasileirao</h1>
-
-
+      <h1>
+      {dados.map((item) => (
+        <div key={item.country_id}>
+          <h2>{item.country_name}</h2>
+          {/* <img src={item.country_logo}/>   */}
+          <Image
+               src={item.country_logo}
+               alt={`baneira do(a) ${item.country_name}`}
+               width={50}
+               height={50}
+              />
+          <p></p>
+        </div>
+      ))}
+      </h1>
     </main>
   )
 }
