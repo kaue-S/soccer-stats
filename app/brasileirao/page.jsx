70b794ccf11b6f618971f10c25401a1ff71b1e25
kaue-S <kauesilva38@hotmail.com>
2024-06-05 10:00:32 -0300

    criação da tabela de classificação

--- app/brasileirao/page.jsx
+++ app/brasileirao/page.jsx
@@ -1,33 +1,42 @@
 
+import ListaLigas from '@/components/ListaLigas';
 import Image from 'next/image';
 import React from 'react'
 
 
 export default async function Brasileirao() {
 
-  // const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
-   const resposta = await fetch('https://apiv3.apifootball.com/?action=get_standings&league_id=99&APIkey=8fb6c79632ee46c334a924cd01221430ce944e9b920eaea51bbbfe887b55bc85')
+   const resposta = await fetch(`https://apiv3.apifootball.com/?action=get_standings&league_id=99&APIkey=${apiKey}`)
    const dados = await resposta.json();
    console.log(dados);
 
 return (
-    <main>
+    <main className="px-8 m-auto">
+      <h1 className="text-center text-3xl">Brasileirão Série A</h1>
+
+      <div className="flex md:justify-center overflow-x-auto">
+        <ListaLigas />
+      </div>
       <h1>
       {dados.map((item) => (
-        <div key={item.country_id}>
-          <h2>{item.country_name}</h2>
+        <div key={item.team_id}>
+          <h2>{item.team_name}</h2>
           {/* <img src={item.country_logo}/>   */}
           <Image
-               src={item.country_logo}
+               src={item.team_badge}
                alt={`baneira do(a) ${item.country_name}`}
                width={50}
                height={50}
               />
-          <p></p>
+          <p>{item.overall_league_position}</p>
+          
         </div>
       ))}
       </h1>
+
+      
     </main>
   )
 }
---
+++ components/ListaLigas.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function ListaLigas() {
+  return (
+    <main>
+        <table className="border">
+            <thead className="bg-gray-400 px-3">
+                <tr>
+                    <th className="pl-3 lg:pr-24 lg:text-xl border">Classificação</th>
+                    <th>P</th>
+                    <th>J</th>
+                    <th>V</th>
+                    <th>E</th>
+                    <th>D</th>
+                    <th>GP</th>
+                    <th>GC</th>
+                    <th>SG</th>
+                    <th>%</th>
+                </tr>
+            </thead>
+            <tbody >
+                <tr >
+                    <td className="border pl-3">lista</td>
+                    <td className="border px-3 bg-gray-200">lista</td>
+                    <td className="border px-3">lista</td>
+                    <td className="border px-3 bg-gray-200">lista</td>
+                    <td className="border px-3">lista</td>
+                    <td className="border px-3 bg-gray-200">lista</td>
+                    <td className="border px-3">lista</td>
+                    <td className="border px-3 bg-gray-200">lista</td>
+                    <td className="border px-3">lista</td>
+                    <td className="border px-3 bg-gray-200">lista</td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+  )
+}
+
